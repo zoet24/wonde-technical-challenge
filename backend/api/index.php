204ade0ee39c251506b42d0ecef472bf0628fbae
Zoe <zt12542@my.bristol.ac.uk>
2023-05-05 12:04:59 +0200

    Add access token and .env file

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,10 +1,24 @@
 <?php
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
 
-$data = [
-    'message' => 'Hello from the PHP backend!'
-];
+// Load in API access token
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
 
-echo json_encode($data);
-exit();
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+$apiAccessToken = $_ENV['API_ACCESS_TOKEN'];
+
+var_dump($apiAccessToken);
+
+// $client = new \Wonde\Client($apiAccessToken);
+
+
+// header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+
+// $data = [
+//     'message' => 'Hello from the PHP backend!'
+// ];
+
+// echo json_encode($data);
+// exit();
